54cb50cf54cbdbe4bf4a999619b09b53d31e736e
fanyang <fanyang@lcworld-inc.com>
2017-08-11 11:06:12 +0800

    智慧后勤

---
+++ app/src/main/java/cn/lc/model/ui/main/presenter/MainPresenter.java
@@ -0,0 +1,42 @@
+package cn.lc.model.ui.main.presenter;
+
+import cn.lc.model.ui.main.model.MainModel;
+import cn.lc.model.ui.main.view.MainView;
+import mvp.cn.rx.MvpRxPresenter;
+import mvp.cn.util.LogUtil;
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * Created by hh on 2017/5/12.
+ */
+
+public class MainPresenter extends MvpRxPresenter<MainModel, MainView> {
+
+
+    public void getData() {
+        LogUtil.log("MainPresenter发出请求");
+        Observable login = getModel().login("", "");
+        getNetWork(login, new Subscriber<Object>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Object loginBean) {
+
+            }
+        });
+    }
+
+    @Override
+    public void detachView(boolean retainInstance) {
+        super.detachView(retainInstance);
+    }
+}
