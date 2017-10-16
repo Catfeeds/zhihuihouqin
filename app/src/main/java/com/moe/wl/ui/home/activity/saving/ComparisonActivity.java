6b7170fe9e021c8fd96c18255909bd60804c218c
wangnengjie <wangnengjie@lcworld-inc.com>
2017-10-16 18:40:10 +0800

    王能杰提交

--- app/src/main/java/com/moe/wl/ui/home/activity/saving/ComparisonActivity.java
+++ app/src/main/java/com/moe/wl/ui/home/activity/saving/ComparisonActivity.java
@@ -1,7 +1,10 @@
 package com.moe.wl.ui.home.activity.saving;
 
+import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.moe.wl.R;
 import com.moe.wl.framework.base.BaseActivity;
@@ -10,18 +13,60 @@ import com.moe.wl.ui.home.modelimpl.saving.ComparisonModelImpl;
 import com.moe.wl.ui.home.presenter.saving.ComparisonPresenter;
 import com.moe.wl.ui.home.view.saving.ComparisonView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import lecho.lib.hellocharts.gesture.ContainerScrollType;
+import lecho.lib.hellocharts.gesture.ZoomType;
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.model.ValueShape;
+import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ColumnChartView;
+import lecho.lib.hellocharts.view.LineChartView;
 
 /**
  * 消耗对比
  */
-public class ComparisonActivity extends BaseActivity<ComparisonModel, ComparisonView, ComparisonPresenter> implements ComparisonView ,View.OnClickListener{
+public class ComparisonActivity extends BaseActivity<ComparisonModel, ComparisonView, ComparisonPresenter> implements ComparisonView, View.OnClickListener {
 
     @BindView(R.id.ll_back)
     LinearLayout llBack;
+    @BindView(R.id.tv_right)
+    TextView tvRight;
+    @BindView(R.id.ll_right)
+    LinearLayout llRight;
+    @BindView(R.id.lineChart)
+    LineChartView lineChart;
+    @BindView(R.id.columnChartc)
+    ColumnChartView columnChartc;
 
+    private int numberOfLines = 1;
+    private int maxNumberOfLines = 4;
+    private int numberOfPoints = 12;
+    float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
+    private boolean hasAxes = true;
+    private boolean hasAxesNames = true;
+    private boolean hasLines = true;
+    private boolean hasPoints = true;
+    private ValueShape shape = ValueShape.CIRCLE;
+    private boolean isFilled = false;
+    private boolean hasLabels = false;
+    private boolean isCubic = false;
+    private boolean hasLabelForSelected = false;
+    private boolean pointsHaveDifferentColor;
+    private boolean hasGradientToTransparent = false;
+
+
+    private LineChartData data;
 
     @Override
     public ComparisonPresenter createPresenter() {
@@ -41,7 +86,115 @@ public class ComparisonActivity extends BaseActivity<ComparisonModel, Comparison
 
     @Override
     public void initView() {
+
+        // Generate some random values.
+        generateValues();
+
+        generateData();
+        lineChart.setViewportCalculationEnabled(false);
+
+        addLineToData();
+
+        columnChartc.setColumnChartData(generateColumnChartData());
+        columnChartc.setZoomType(ZoomType.HORIZONTAL);
+
+        columnChartc.setContainerScrollEnabled(true, ContainerScrollType.HORIZONTAL);
+
+
+
     }
+
+    private void generateValues() {
+        for (int i = 0; i < maxNumberOfLines; ++i) {
+            for (int j = 0; j < numberOfPoints; ++j) {
+                randomNumbersTab[i][j] = (float) Math.random() * 100f;
+            }
+        }
+    }
+
+    private void addLineToData() {
+        if (data.getLines().size() >= maxNumberOfLines) {
+            Toast.makeText(getActivity(), "Samples app uses max 4 lines!", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            ++numberOfLines;
+        }
+
+        generateData();
+    }
+
+
+    private ColumnChartData generateColumnChartData() {
+        int numSubcolumns = 1;
+        int numColumns = 12;
+        // Column can have many subcolumns, here by default I use 1 subcolumn in each of 8 columns.
+        List<Column> columns = new ArrayList<Column>();
+        List<SubcolumnValue> values;
+        for (int i = 0; i < numColumns; ++i) {
+
+            values = new ArrayList<SubcolumnValue>();
+            for (int j = 0; j < numSubcolumns; ++j) {
+                values.add(new SubcolumnValue((float) Math.random() * 50f + 5, ChartUtils.pickColor()));
+            }
+
+            columns.add(new Column(values));
+        }
+
+        ColumnChartData data = new ColumnChartData(columns);
+
+        data.setAxisXBottom(new Axis().setName("Axis X"));
+        data.setAxisYLeft(new Axis().setName("Axis Y").setHasLines(true));
+        return data;
+
+    }
+
+    private void generateData() {
+
+        List<Line> lines = new ArrayList<Line>();
+        for (int i = 0; i < numberOfLines; ++i) {
+
+            List<PointValue> values = new ArrayList<PointValue>();
+            for (int j = 0; j < numberOfPoints; ++j) {
+                values.add(new PointValue(j, randomNumbersTab[i][j]));
+            }
+
+            Line line = new Line(values);
+            line.setColor(ChartUtils.COLORS[i]);
+            line.setShape(shape);
+            line.setCubic(isCubic);
+            line.setFilled(isFilled);
+            line.setHasLabels(hasLabels);
+            line.setHasLabelsOnlyForSelected(hasLabelForSelected);
+            line.setHasLines(hasLines);
+            line.setHasPoints(hasPoints);
+            line.setHasGradientToTransparent(hasGradientToTransparent);
+            if (pointsHaveDifferentColor){
+                line.setPointColor(ChartUtils.COLORS[(i + 1) % ChartUtils.COLORS.length]);
+            }
+            lines.add(line);
+        }
+
+        data = new LineChartData(lines);
+
+        if (hasAxes) {
+            Axis axisX = new Axis();
+            Axis axisY = new Axis().setHasLines(true);
+            if (hasAxesNames) {
+                axisX.setName("Axis X");
+                axisY.setName("Axis Y");
+            }
+            data.setAxisXBottom(axisX);
+            data.setAxisYLeft(axisY);
+        } else {
+            data.setAxisXBottom(null);
+            data.setAxisYLeft(null);
+        }
+
+        data.setBaseValue(Float.NEGATIVE_INFINITY);
+        lineChart.setLineChartData(data);
+
+    }
+
 
     @OnClick({R.id.ll_back})
     @Override
@@ -56,5 +209,12 @@ public class ComparisonActivity extends BaseActivity<ComparisonModel, Comparison
 
     @Override
     public void setData() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
